c05dbca765e14808175dc3c20b451278019283ec
shommy14 <36086060+shommy14@users.noreply.github.com>
2019-12-16 10:19:28 +0100

    Queued Mailable

--- app/Mail/ContactFormMail.php
+++ app/Mail/ContactFormMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class ContactFormMail extends Mailable
+class ContactFormMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
     public $data;
