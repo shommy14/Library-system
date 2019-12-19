2eb76d6d127d73c655a4722acc7aa788ddf6500a
shommy14 <36086060+shommy14@users.noreply.github.com>
2019-12-19 19:03:37 +0100

    Task Scheduling
    Reminds users who has not returned book by daily message to check their dashboard.

---
+++ app/Console/Commands/RemindUsersCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Borrow;
+use App\Notifications\RemindUserAboutBookDueDate;
+
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Carbon;
+
+class RemindUsersCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'book:reminder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remind users about book due date';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $usersId = Borrow::whereDate('dueDate','>',Carbon::now())->pluck('userId');
+        $users = User::findMany($usersId->toArray());
+
+        Notification::send($users, new RemindUserAboutBookDueDate());
+    }
+}
